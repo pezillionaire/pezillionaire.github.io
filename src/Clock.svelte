9c1f2a3cb9681437d7d67f7e25a939c7d007cfb2
Andrew (Pez) Pengelly <hello@pezillionaire.com>
2021-01-23 20:44:35 -0800

    init upload

---
+++ src/Clock.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onMount } from 'svelte';
+	let date = new Date();
+	$: hour = date.getHours();
+	$: getMin = date.getMinutes();
+  let dayOrNight = 'AM';
+  let min = 30;
+
+	onMount( () => {
+		const interval = setInterval(() => {
+			date = new Date();
+      dayOrNight = (hour >= 12) ? "PM" : "AM";
+      min = (getMin >= 10) ? getMin : `0${getMin}`;
+		}, 1000);
+	});
+</script>
+<div class="clock">{hour - 12}:{min}{dayOrNight}</div>
