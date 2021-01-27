4ccbf3c546438942eeb7c9af1d9053772b0d2fdf
Andrew (Pez) Pengelly <hello@pezillionaire.com>
2021-01-26 18:18:57 -0800

    fix desktop alignment issues

--- src/Desktop.svelte
+++ src/Desktop.svelte
@@ -2,29 +2,6 @@
   import Window from "./Window.svelte";
 </script>
 
-<style>
-  .desktop {
-    background-image: linear-gradient(
-        45deg,
-        var(--primary) 25%,
-        transparent 25%,
-        transparent 75%,
-        var(--primary) 75%,
-        var(--primary)
-      ),
-      linear-gradient(
-        45deg,
-        var(--primary) 25%,
-        transparent 25%,
-        transparent 75%,
-        var(--primary) 75%,
-        var(--primary)
-      );
-    background-size: 4px 4px;
-    background-position: 0 0, 2px 2px;
-  }
-</style>
-
 <Window />
 
 <section class="desktop">
