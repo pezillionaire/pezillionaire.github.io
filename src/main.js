4cb0aaf768d5262b18c836a711b3f20775ddc4c1
Andrew (Pez) Pengelly <hello@pezillionaire.com>
2021-02-01 09:00:30 -0800

    switch to single app model

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
 import Desktop from './Desktop.svelte';
-import Clock from './Clock.svelte';
-import Nav from './Nav.svelte';
 
 function replaceContents(node) {
   node.innerHTML = '';
   return node;
 }
-
-var nav = new Nav({
-  target: replaceContents(document.querySelector('#nav')),
-});
-
-var clock = new Clock({
-  target: replaceContents(document.querySelector('#clock')),
-});
 
 var desktop = new Desktop({
   target: replaceContents(document.querySelector('#desktop')),
@@ -21,4 +11,4 @@ var desktop = new Desktop({
 
 
 
-export default { clock, desktop, nav };
+export default { desktop };
